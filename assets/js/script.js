862a956671c93cc6eef5149193296aa2bd69c8f4
ravihamidov <ravihamidov42@gmail.com>
2022-05-09 01:23:53 +0400

    GitHub Organize

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
 let sum = 0;
-
 function plus() {
     sum++;
     document.querySelector(".Num span").innerHTML = sum;
 }
-
 function minus() {
     sum--;
     document.querySelector(".Num span").innerHTML = sum;
 }
-
 function reset() {
     sum = 0;
     document.querySelector(".Num span").innerHTML = sum;
