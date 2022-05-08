2c1b31b16c9bb73947205f419f29028f4f859300
ravihamidov <ravihamidov42@gmail.com>
2022-05-08 23:42:26 +0400

    add Files

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+## [Counter](https://ravihamidov.github.io/Counter/)
+
+- İşdə istifadə olunan texnologiyalar: HTML5, CSS3, JavaScript.
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let sum = 0;
+
+function plus() {
+    sum++;
+    document.querySelector(".Num span").innerHTML = sum;
+}
+
+function minus() {
+    sum--;
+    document.querySelector(".Num span").innerHTML = sum;
+}
+
+function reset() {
+    sum = 0;
+    document.querySelector(".Num span").innerHTML = sum;
+}
