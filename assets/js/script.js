e5dd83b216d1ff4afd3beb37dd4aeb040405cd63
ravihamidov <ravihamidov42@gmail.com>
2022-05-29 15:38:03 +0400

    File Organizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 let sum = 0;
 function plus() {
     sum++;
-    document.querySelector(".Num span").innerHTML = sum;
+    document.querySelector(".num span").innerHTML = sum;
 }
 function minus() {
     sum--;
-    document.querySelector(".Num span").innerHTML = sum;
+    document.querySelector(".num span").innerHTML = sum;
 }
 function reset() {
     sum = 0;
-    document.querySelector(".Num span").innerHTML = sum;
+    document.querySelector(".num span").innerHTML = sum;
 }
